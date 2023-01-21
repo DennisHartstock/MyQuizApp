ab9f2e3a62bb722cc292fbb9aa8f8dbcba4b099f
DennsHartstock <dennis.hartstock@web.de>
2023-01-21 12:33:52 +0100

    implement btSubmit

--- app/src/main/java/com/commcode/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/commcode/myquizapp/QuizQuestionsActivity.kt
@@ -11,7 +11,9 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition = 0
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedOptionPosition = 0
+    private var countCorrectAnswers = 0
 
+    private lateinit var newQuestion: Question
     private lateinit var pbCountQuestions: ProgressBar
     private lateinit var tvCountQuestions: TextView
     private lateinit var tvQuestion: TextView
@@ -30,14 +32,47 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         setNewQuestion()
 
         btSubmit.setOnClickListener {
-            Toast.makeText(this, "Button submit clicked", Toast.LENGTH_SHORT).show()
+            if (mSelectedOptionPosition != 0) {
+                if (newQuestion.correctAnswer != mSelectedOptionPosition) {
+                    answerView(mSelectedOptionPosition, R.drawable.wrong_answer_bg)
+                } else {
+                    countCorrectAnswers++
+                }
+                answerView(newQuestion.correctAnswer, R.drawable.correct_answer_bg)
+                btSubmit.text = "next Question"
+                mSelectedOptionPosition = 0
+
+            } else if (mCurrentPosition < mQuestionsList!!.size - 1) {
+                mCurrentPosition++
+                setNewQuestion()
+            } else {
+                pbCountQuestions.progress = 0
+                tvCountQuestions.text = "$countCorrectAnswers/${pbCountQuestions.max}"
+                Toast.makeText(
+                    this,
+                    "You have $countCorrectAnswers of ${pbCountQuestions.max} correct answers",
+                    Toast.LENGTH_LONG
+                ).show()
+                btSubmit.text = "new game"
+                mCurrentPosition = 0
+                countCorrectAnswers = 0
+            }
+        }
+    }
+
+    private fun answerView(answer: Int, drawable: Int) {
+        when (answer) {
+            1 -> tvOption1.background = ContextCompat.getDrawable(this, drawable)
+            2 -> tvOption2.background = ContextCompat.getDrawable(this, drawable)
+            3 -> tvOption3.background = ContextCompat.getDrawable(this, drawable)
+            4 -> tvOption4.background = ContextCompat.getDrawable(this, drawable)
         }
     }
 
     private fun setNewQuestion() {
-        val newQuestion: Question = mQuestionsList!![mCurrentPosition]
+        newQuestion = mQuestionsList!![mCurrentPosition]
         pbCountQuestions.progress = mCurrentPosition
-        val countQuestions = "$mCurrentPosition/${pbCountQuestions.max}"
+        val countQuestions = "$countCorrectAnswers/${pbCountQuestions.max}"
         tvCountQuestions.text = countQuestions
         tvQuestion.text = newQuestion.question
         ivFlag.setImageResource(newQuestion.image)
@@ -45,8 +80,11 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOption2.text = newQuestion.option2
         tvOption3.text = newQuestion.option3
         tvOption4.text = newQuestion.option4
-
-        if (mCurrentPosition == pbCountQuestions.max) btSubmit.text = getString(R.string.bt_finish)
+        tvOption1.setBackgroundResource(android.R.color.darker_gray)
+        tvOption2.setBackgroundResource(android.R.color.darker_gray)
+        tvOption3.setBackgroundResource(android.R.color.darker_gray)
+        tvOption4.setBackgroundResource(android.R.color.darker_gray)
+        btSubmit.text = "submit"
     }
 
     private fun initViews() {
